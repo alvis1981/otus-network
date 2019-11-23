8c8c984107e124f2e2c52baf85a5bf60edb89947
a.kryachko <a.kryachko@hlebnitca.ru>
2019-11-23 12:03:40 +0400

    home work 4

--- .docker/mysql/mysql_connector.sh
+++ .docker/mysql/mysql_connector.sh
@@ -11,12 +11,12 @@ echo "-----------------"
 
 echo "* Create replication user"
 
-mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'STOP SLAVE;';
-mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'RESET SLAVE ALL;';
+#mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'STOP SLAVE;';
+#mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'RESET SLAVE ALL;';
 
-mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "CREATE USER '$MYSQL_REPLICATION_USER'@'%';"
-mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPLICATION_USER'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASSWORD';"
-mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'flush privileges;'
+#mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "CREATE USER '$MYSQL_REPLICATION_USER'@'%';"
+#mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "GRANT REPLICATION SLAVE ON *.* TO '$MYSQL_REPLICATION_USER'@'%' IDENTIFIED BY '$MYSQL_REPLICATION_PASSWORD';"
+#mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'flush privileges;'
 
 
 echo "* Set MySQL01 as master on MySQL02"
@@ -29,25 +29,40 @@ mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "CHANGE MASTER TO 
         master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL01_File', \
         master_log_pos=$MYSQL01_Position;"
 
+mysql --host mysqlslave2 -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "CHANGE MASTER TO master_host='mysql', master_port=3306, \
+        master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL01_File', \
+        master_log_pos=$MYSQL01_Position;"
+
+
 echo "* Set MySQL02 as master on MySQL01"
 
 MYSQL02_Position=$(eval "mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
 MYSQL02_File=$(eval "mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
 
+MYSQL03_Position=$(eval "mysql --host mysqlslave2 -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G' | grep Position | sed -n -e 's/^.*: //p'")
+MYSQL03_File=$(eval "mysql --host mysqlslave2 -uroot -p$MYSQL_SLAVE_PASSWORD -e 'show master status \G'     | grep File     | sed -n -e 's/^.*: //p'")
+
 SLAVE_IP=$(eval "getent hosts mysqlslave|awk '{print \$1}'")
+SLAVE2_IP=$(eval "getent hosts mysqlslave2|awk '{print \$1}'")
 echo $SLAVE_IP
 mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "CHANGE MASTER TO master_host='mysqlslave', master_port=3306, \
         master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL02_File', \
         master_log_pos=$MYSQL02_Position;"
+mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e "CHANGE MASTER TO master_host='mysqlslave2', master_port=3306, \
+        master_user='$MYSQL_REPLICATION_USER', master_password='$MYSQL_REPLICATION_PASSWORD', master_log_file='$MYSQL03_File', \
+        master_log_pos=$MYSQL03_Position;"
 
 echo "* Start Slave on both Servers"
 mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "start slave;"
+mysql --host mysqlslave2 -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e "start slave;"
 
 echo "Increase the max_connections to 2000"
 mysql --host mysql -uroot -p$MYSQL_MASTER_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
 mysql --host mysqlslave -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
+mysql --host mysqlslave2 -uroot -p$MYSQL_SLAVE_PASSWORD -AN -e 'set GLOBAL max_connections=2000';
 
 mysql --host mysqlslave -uroot -p$MYSQL_MASTER_PASSWORD -e "show slave status \G"
+mysql --host mysqlslave2 -uroot -p$MYSQL_MASTER_PASSWORD -e "show slave status \G"
 
 echo "MySQL servers created!"
 echo "--------------------"
@@ -56,3 +71,4 @@ echo Variables available fo you :-
 echo
 echo MYSQL01_IP       : mysql
 echo MYSQL02_IP       : mysqlslave
+echo MYSQL03_IP       : mysqlslave2
